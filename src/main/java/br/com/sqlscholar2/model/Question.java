6d0e7b45908e575b6aa2287439aa4efec6c4411e
henriquemachado046 <henriquemachado046@yahoo.com>
2025-08-16 02:11:24 -0300

    Criados atributos de Question e QuestionList.

--- src/main/java/br/com/sqlscholar2/model/Question.java
+++ src/main/java/br/com/sqlscholar2/model/Question.java
@@ -1,5 +1,52 @@
 package br.com.sqlscholar2.model;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.json.bind.Jsonb;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "question")
 public class Question {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @PrimaryKeyJoinColumn
+    private UUID id;
+    
+    @Column
+    private String title;
+ 
+    @Column
+    private String description;
+    
+    @Column
+    private Jsonb sql;
+    
+    @Column
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+    @ManyToOne
+    @JoinColumn(name = "questionlist_id")
+    private QuestionList questionList;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne
+    private User owner;
+    
+    @Column
+    private String difficulty;
     
 }
--- src/main/java/br/com/sqlscholar2/model/QuestionList.java
+++ src/main/java/br/com/sqlscholar2/model/QuestionList.java
@@ -1,5 +1,50 @@
 package br.com.sqlscholar2.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.json.bind.Jsonb;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+
 public class QuestionList {
-        
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @PrimaryKeyJoinColumn
+    private UUID id;
+    
+    @OneToMany(cascade = CascadeType.REMOVE, mappedBy = "questionList")
+    private List<Question> questions = new ArrayList<>();
+    
+    @Column
+    private String databaseName;
+
+    @Column
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+    @Column
+    private String title;
+    
+    @Column
+    private Jsonb databaseScript;
+
+    @Column
+    private String description;
+    
+    @Column
+    private boolean isPrivate;    
+    
+    @ManyToOne
+    private User owner;    
 }
